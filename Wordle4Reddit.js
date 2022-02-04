090a2ec71adb33623f27c6bf53cb54a72d6e4f5e
KataiKi <kataisf@gmail.com>
2022-02-04 16:44:37 -0700

    Update Wordle4Reddit.js

--- Wordle4Reddit.js
+++ Wordle4Reddit.js
@@ -1,10 +1,17 @@
 javascript:(function(){
   
-  var input= document.getElementsByTagName("textarea")[0].value.split("\n")
+  var input;
+  var elem;
+  
+  if(elem = document.querySelector('textarea')) input = elem.value.split("\n"); // Old Reddit
+  else if (elem = document.querySelector('span[data-text="true"]')) input = elem.innerHTML.split("\n"); // New Reddit
+  
   var output = input.slice(0,2);
   for( i = 2; i < input.length; i++){
     output.push(input[i].split(" ")[0] + " >!" + input[i].split(" ")[1] + "!<", "")
   }
-  document.getElementsByTagName("textarea")[0].value = output.join("\n");
+  
+  if(elem.tagName == "TEXTAREA") elem.value = output.join("\n"); // Old Reddit
+  else if(elem.tagName == "SPAN") elem.innerHTML = output.join("\n"); // New Reddit
 
 })();
