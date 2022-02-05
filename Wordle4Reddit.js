305ec9e61c9563e5b3795a79705bccf78a201d2b
KataiKi <kataisf@gmail.com>
2022-02-05 01:23:44 -0700

    Update Wordle4Reddit.js

--- Wordle4Reddit.js
+++ Wordle4Reddit.js
@@ -8,7 +8,7 @@ javascript:(function(){
   
   var output = input.slice(0,2);
   for( i = 2; i < input.length; i++){
-    output.push(input[i].match(/[^a-zA-Z]+/)[0].trim() + " >!" + (input[i].match(/[a-zA-Z]+/)[0] || "") + "!<", "")
+    output.push(input[i].match(/[^a-zA-Z]+/)[0].trim() + " >!" + (input[i].match(/[a-zA-Z]+/) || "") + "!<", "")
   }
   
   if(elem.tagName == "TEXTAREA") { elem.value = output.join("\n"); } // Old Reddit
